0d10ad61473ffbac6ca280d20548ed9701ba956c
280099 <280099@student.pwr.edu.pl>
2025-11-13 15:13:29 +0100

    Added project

---
+++ src/main/java/com/pubito/pubito_backend/PubitoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pubito.pubito_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubitoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PubitoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pubito/pubito_backend/PubitoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pubito.pubito_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PubitoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
