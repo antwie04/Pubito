ee0ee95fd3bcea31b9a2aef8d27d89cefcb7f04c
280099 <280099@student.pwr.edu.pl>
2025-12-09 23:03:24 +0100

    details

--- src/main/java/com/pubito/pubito_backend/repositories/MenuRepository.java
+++ src/main/java/com/pubito/pubito_backend/repositories/MenuRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
+
     List<Menu> findByBarId(Long barId);
 
     List<Menu> findTop3ByBarIdOrderByPriceAsc(Long barId);
